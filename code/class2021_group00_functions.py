d921d1bd25829cdfec35c62a89b349c8f0b3adea
Mark Janse <mjanse2@umcutrecht.nl>
2021-11-18 21:15:12 +0100

    Small code update for 2021

--- code/class2021_group00_functions.py
+++ code/class2021_group00_functions.py
@@ -57,15 +57,20 @@ def knn_classifier(X_train, y_train, X_validation, X_test, k):
     # y_pred_test - num_test x 1 predicted vector with labels for the test data
 
     X_test_val = np.vstack((X_validation, X_test))
+
     # Compute standardized euclidian distance of validation and test points to the other points
     D = cdist(X_test_val, X_train, metric='seuclidean')
+
     # Sort distances per row and return array of indices from low to high
     sort_ix = np.argsort(D, axis=1)
+
     # Get the k smallest distances
     sort_ix_k = sort_ix[:, :k]
     predicted_labels = y_train[sort_ix_k]
+
     # Predictions for each point is the mode of the K labels closest to the point
     predicted_labels = mode(predicted_labels, axis=1)[0]
     y_pred_validation = predicted_labels[:len(X_validation)]
     y_pred_test = predicted_labels[len(X_validation):]
+
     return y_pred_validation, y_pred_test
